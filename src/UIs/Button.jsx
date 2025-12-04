9d4a116a4c071c718c22b5435739d0a8074be8df
Shahidul Islam <89517917+shahidul1920@users.noreply.github.com>
2025-12-04 21:46:26 +0600

    damn ui bug

--- src/UIs/Button.jsx
+++ src/UIs/Button.jsx
@@ -16,13 +16,14 @@ const Button = (params) => {
   })
 
   return (
-    <div ref={containerRef}>
-      <button onMouseEnter={hoverHandler} onMouseLeave={hoverLeave} className="btn px-4 py-2 rounded-full text-[16px] flex gap-1 items-center text-[var(--light)] cursor-pointer">
+    <div ref={containerRef} className='relative rounded-full bg-gray-500'>
+      <button onMouseEnter={hoverHandler} onMouseLeave={hoverLeave} className="btn overflow-hidden px-4 py-2 rounded-full text-[16px] flex gap-1 items-center cursor-pointer">
         {params.children}
         {/* <img ref={starref} className='w-[20px] h-auto star' src="./star.svg" alt="" /> */}
-        <svg ref={starref} className='w-[20px] h-auto star' viewBox="0 0 31 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <svg ref={starref} className='w-[20px] h-auto star' viewBox="0 0 31 31" xmlns="http://www.w3.org/2000/svg">
           <path d="M15.5 31C15.469 22.4501 8.54135 15.5287 0 15.5287C8.56041 15.5287 15.5 8.57619 15.5 0C15.5309 8.54988 22.4586 15.4713 31 15.4713C22.4394 15.4713 15.5 22.4238 15.5 31Z" fill="white" />
         </svg>
+        
       </button>
     </div>
   )
