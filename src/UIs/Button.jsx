2f7ba1244042d3cff6c18b84f33777c32f1645be
Shahidul Islam <89517917+shahidul1920@users.noreply.github.com>
2025-12-04 21:11:01 +0600

    Add GSAP star rotation animation to Button component
    Integrated GSAP and @gsap/react dependencies and updated the Button component to animate the star icon with a 360-degree rotation on hover. This enhances the button's interactivity and visual feedback.

--- src/UIs/Button.jsx
+++ src/UIs/Button.jsx
@@ -1,12 +1,21 @@
-import React from 'react'
+import gsap from 'gsap'
+import { useGSAP } from '@gsap/react'
+import { useRef } from 'react'
 
 const Button = (params) => {
+
+  const starref = useRef(null)
+
+  const hoverHandler = () => {
+    gsap.to(starref.current, { rotation: "+=360", duration: 1, ease: "power2.out", transformOrigin: "50% 50%" })
+  }
+
   return (
     <div>
-        <button className="btn px-4 py-2 rounded-full text-[16px] text-[var(--text-dark)] flex gap-1 items-center text-[var(--light)]">
-            {params.children}
-            <img className='w-[20px] h-auto' src="./star.svg" alt="" />
-        </button>
+      <button onMouseEnter={hoverHandler} className="btn px-4 py-2 rounded-full text-[16px] flex gap-1 items-center text-[var(--light)] cursor-pointer">
+        {params.children}
+        <img ref={starref} className='w-[20px] h-auto star' src="./star.svg" alt="" />
+      </button>
     </div>
   )
 }
