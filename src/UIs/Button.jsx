67a64b3cab96d52842eca6aa47c22c5138dacc83
Shahidul Islam <89517917+shahidul1920@users.noreply.github.com>
2025-12-04 21:33:00 +0600

    Enhance Button animation and add transition to styles
    Refactored Button component to use GSAP context for animation, added rotation reset on mouse leave, and replaced the star image with an inline SVG. Added a transition property to button styles in SCSS and CSS for smoother visual effects.

--- src/UIs/Button.jsx
+++ src/UIs/Button.jsx
@@ -3,18 +3,26 @@ import { useGSAP } from '@gsap/react'
 import { useRef } from 'react'
 
 const Button = (params) => {
-
+  const containerRef = useRef(null)
   const starref = useRef(null)
 
-  const hoverHandler = () => {
-    gsap.to(starref.current, { rotation: "+=360", duration: 1, ease: "power2.out", transformOrigin: "50% 50%" })
-  }
+  const { contextSafe } = useGSAP({ scope: containerRef })
+
+  const hoverHandler = contextSafe(() => {
+    gsap.to(starref.current, { rotation: 360, duration: 1, ease: "power2.out", transformOrigin: "50% 50%" })
+  })
+  const hoverLeave = contextSafe(() => {
+    gsap.to(starref.current, { rotation: 0, duration: 1, ease: "power2.out", transformOrigin: "50% 50%" })
+  })
 
   return (
-    <div>
-      <button onMouseEnter={hoverHandler} className="btn px-4 py-2 rounded-full text-[16px] flex gap-1 items-center text-[var(--light)] cursor-pointer">
+    <div ref={containerRef}>
+      <button onMouseEnter={hoverHandler} onMouseLeave={hoverLeave} className="btn px-4 py-2 rounded-full text-[16px] flex gap-1 items-center text-[var(--light)] cursor-pointer">
         {params.children}
-        <img ref={starref} className='w-[20px] h-auto star' src="./star.svg" alt="" />
+        {/* <img ref={starref} className='w-[20px] h-auto star' src="./star.svg" alt="" /> */}
+        <svg ref={starref} className='w-[20px] h-auto star' viewBox="0 0 31 31" fill="none" xmlns="http://www.w3.org/2000/svg">
+          <path d="M15.5 31C15.469 22.4501 8.54135 15.5287 0 15.5287C8.56041 15.5287 15.5 8.57619 15.5 0C15.5309 8.54988 22.4586 15.4713 31 15.4713C22.4394 15.4713 15.5 22.4238 15.5 31Z" fill="white" />
+        </svg>
       </button>
     </div>
   )
