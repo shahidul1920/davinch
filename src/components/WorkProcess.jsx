74d9d9e4e687401fe1f28d62fb2ffa0d29d0a0f5
Shahidul Islam <89517917+shahidul1920@users.noreply.github.com>
2025-12-06 00:49:51 +0600

    Add eatface.png and update WorkProcess steps
    Added eatface.png image to the public directory. Updated WorkProcess.jsx to include a rotated image for each step and adjusted layout spacing for improved visual presentation.

--- src/components/WorkProcess.jsx
+++ src/components/WorkProcess.jsx
@@ -5,16 +5,19 @@ const WorkProcess = () => {
 
     const steps = [
         {
+            rotate: "0deg",
             number: "1",
             title: "Step 1: Product design Research",
             desc: "This initial step involves a systematic and thorough exploration of the market landscape."
         },
         {
+            rotate: "90deg",
             number: "2",
             title: "Step 2: UI design in figma",
             desc: "We focus on creating a user interface that is intuitive efficient, and enjoyable to use."
         },
         {
+            rotate: "-90deg",
             number: "3",
             title: "Step 3: No-code develop in webflow",
             desc: "Bringing the design to life using Webflow development for pixel-perfect implementation."
@@ -31,14 +34,15 @@ const WorkProcess = () => {
                 </div>
             </div>
 
-            <div className="container mx-auto grid gap-8 mt-[1rem] md:mt-[4rem]">
+            <div className="container mx-auto grid gap-6 mt-[1rem] md:mt-[4rem]">
                 {steps.map((step, i) => (
-                    <div key={i} className='flex w-full'>
+                    <div key={i} className='flex w-full gap-7'>
                         <div className="left w-[80px] flex flex-col items-center text-center justify-center gap-2">
                             <div className="number text-[22px] grid place-content-center font-regular text-[var(--primary)] border border-white rounded-full min-w-[40px] min-h-[40px]">{step.number}</div>
                             <div className="line w-[1.4px] h-full bg-[#3c4141]" />
                         </div>
                         <div className="right grid gap-4">
+                            <img className={`w-[35px] h-auto rotate-[${step.rotate}]`} src="./eatface.png" alt="" />
                             <h3 className='text-[22px] font-regular'>{step.title}</h3>
                             <p className='text-[19px] max-w-[580px] font-thin text-[var(--lightGray)]'>{step.desc}</p>
                             <Button className="mt-4">Learn More</Button>
