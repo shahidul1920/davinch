8a77845857c0a40b379303658a3fc39abc276c75
Shahidul Islam <89517917+shahidul1920@users.noreply.github.com>
2025-12-29 23:31:20 +0600

    up

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -1,4 +1,3 @@
-import React from 'react'
 import Heading from '../UIs/Heading'
 import Button from '../UIs/Button'
 
@@ -10,9 +9,13 @@ const CTA = () => {
             <div className="container mx-auto py-28">
                 <div className="text">
                     <Heading>Ready to get started?</Heading>
-                    <p className='text-white text-[18px] font-light mt-6 max-w-2xl'>Join thousands of businesses using our services to boost their online presence and drive growth. Take the first step towards transforming your digital strategy today!</p>
+                    <p className='text-white text-[18px] font-light mt-6 max-w-2xl'>
+                        Join thousands of businesses using our services to boost their online presence and drive growth. Take the first step towards transforming your digital strategy today!
+                    </p>
                     <div className="mt-6">
-                        <Button>Get Started Now</Button>
+                        <Button>
+                            Get Started Now                            
+                        </Button>
                     </div>
                 </div>
             </div>
