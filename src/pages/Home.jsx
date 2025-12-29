f79714a79ba1b90b97245916cf3eb1f8a4b808a1
Shahidul Islam <89517917+shahidul1920@users.noreply.github.com>
2025-12-30 01:35:12 +0600

    weird, isn't it?

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,9 @@
 import React from 'react'
+import Hero from '../components/Hero'
+import Featured from '../components/Featured'
+import WorkProcess from '../components/WorkProcess'
+import Testimonials from '../components/Testimonials'
+import CTA from '../components/CTA'
 
 export const Home = () => {
     return (
