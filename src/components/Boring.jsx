34e1d30bb75b4b0249ebb016d5b7c7865718f44b
Shahidul Islam <89517917+shahidul1920@users.noreply.github.com>
2026-01-09 01:06:15 +0600

    imgs

--- src/components/Boring.jsx
+++ src/components/Boring.jsx
@@ -3,11 +3,17 @@ import React from 'react'
 export const Boring = () => {
   return (
     <div>
-        <div className="container mx-auto py-20 px-6 md:px-0">
+        <div className="container mx-auto py-20 px-6 md:px-0 relative">
             <div className="text-center text-white text-3xl uppercase font-semibold headingFont">
                 <h2>tired of boring websites that</h2>
                 <h2>fail to engage and inspire?</h2>
                 <h2>Look no further.</h2>
+            </div>
+            <div className="uiImgs absolute">
+                <img className='mt-4 w-[140px]' src="./subImg.png" alt="" />
+                <img className='mt-4 w-[140px]' src="./subImg1.png" alt="" />
+                <img className='mt-4 w-[140px]' src="./subImg2.png" alt="" />
+                <img className='mt-4 w-[140px]' src="./subImg3.png" alt="" />
             </div>
         </div>
     </div>
