174b65a77d56c5ed73f2650c7d11e6a5fc704ade
Shahidul Islam <89517917+shahidul1920@users.noreply.github.com>
2026-01-12 01:59:54 +0600

    Add GSAP scroll animations to Boring component
    Introduces GSAP and useGSAP to animate images in the Boring component based on scroll position. Images are grouped and animated with staggered movement and initial transforms, enhancing the visual interactivity of the UI section.

--- src/components/Boring.jsx
+++ src/components/Boring.jsx
@@ -1,6 +1,48 @@
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap'
 import React from 'react'
 
 export const Boring = () => {
+  useGSAP(()=>{
+    const uiImagesL = gsap.utils.toArray('.uiImgs .imgL');
+    const uiImagesR = gsap.utils.toArray('.uiImgs .imgR');
+    gsap.set(uiImagesL[0], {
+      x: 400,
+      y: -150,
+      rotation: -15
+    });
+    gsap.set(uiImagesL[1], {
+      x: 380,
+      y: -120,
+      rotation: 15
+    })
+    gsap.set(uiImagesL, {
+      scale: 0.9
+    })
+    gsap.set(uiImagesR, {x: 0});
+    gsap.timeline({
+      scrollTrigger:{
+        trigger: '.uiImgs',
+        start: 'top 60%',
+        end: 'bottom 20%',
+        scrub: 1,
+        markers: true
+      }
+    })
+    .to(uiImagesL,{
+      x: "-=100",
+      stagger: 0.2,
+      ease: 'none',
+      duration: 1
+    })
+    .to(uiImagesR,{
+      x: '-=200',
+      stagger: 0.2,
+      ease: 'none',
+      duration: 1
+    })
+  })
+
   return (
     <div>
         <div className="container mx-auto py-20 px-6 md:px-0 relative">
@@ -10,10 +52,10 @@ export const Boring = () => {
                 <h2>Look no further.</h2>
             </div>
             <div className="uiImgs absolute">
-                <img className='mt-4 w-[140px]' src="./subImg.png" alt="" />
-                <img className='mt-4 w-[140px]' src="./subImg1.png" alt="" />
-                <img className='mt-4 w-[140px]' src="./subImg2.png" alt="" />
-                <img className='mt-4 w-[140px]' src="./subImg3.png" alt="" />
+                <img className='mt-4 w-[140px] imgL' src="./subImg.png" alt="" />
+                <img className='mt-4 w-[140px] imgL' src="./subImg1.png" alt="" />
+                <img className='mt-4 w-[140px] imgR' src="./subImg2.png" alt="" />
+                <img className='mt-4 w-[140px] imgR' src="./subImg3.png" alt="" />
             </div>
         </div>
     </div>
